18cb874af43dc29b05e0a968083b2145c11b765d
wittaya khuanwilai <wittaya.khuanwilai@hotmail.com>
2026-06-30 12:39:23 +0700

    Update menu.blade.php

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('admin.home') }}" class="brand-link">
         <img src="{{ url('image/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-gray">ระบบบริหารการจัดสอบ</span>
+        <span class="brand-text font-weight-gray">PMC Exam</span>
     </a>
     {{-- <a href="{{ route('admin.home') }}" class="brand-link">
     <span class="brand-text font-weight-gray">ระบบบริหารการจัดสอบ</span>
