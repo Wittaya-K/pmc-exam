f807bd59fa457c7e366c18e2ae24ab7896ac1d8c
wittaya khuanwilai <wittaya.khuanwilai@hotmail.com>
2026-07-16 11:26:18 +0700

    fix merge branch

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd /var/www/pmc-exam
-git pull origin dev-pmc-14-07-2569
+git pull origin dev-pmc-15-07-2569
 docker compose up -d --build
 docker exec pmc-exam-app php artisan optimize:clear
 docker exec pmc-exam-app php artisan config:cache
